dfb40360f0de9d8d86c64a1088c6731da8405193
tannoah <tannoah@me.com>
2025-04-04 00:33:03 +0800

    tidying up sign in

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,6 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
+const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL.endsWith('/')
+    ? `${import.meta.env.VITE_BACK_END_SERVER_URL.slice(0, -1)}/auth`
+    : `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 const signUp = async (formData) => {
   try {
